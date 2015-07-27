af19259b6994e7508788528a399a20e5c1f757a3
Andrew <chenpoanandrew@gmail.com>
2015-07-27 21:19:43 +0800

    add hover envent

---
+++ devServer/public/asset/js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$('html, body').on('mouseover', 'img', function(){
+
+		var index = $(this).attr('data-index'); //id
+
+		$('#service' + index).animate({
+		    'zIndex': 3
+		}, 0);
+
+		$( "#tab" + index ).animate({
+		    'zIndex': 2
+		}, 0).animate({
+		    opacity: 1,
+		}, 200);
+	});
+
+	$('html, body').on('mouseleave', '.tab', function(){
+
+		var index = $(this).attr('data-index'); //id
+
+		$( "#tab" + index ).animate({
+			opacity: 0
+		}, 150, function(){
+			$(this).animate({ 'zIndex': 0 }, 0)
+			$('#service' + index).animate({
+			    'zIndex': 1
+			}, 0);
+		});
+	});
+});
