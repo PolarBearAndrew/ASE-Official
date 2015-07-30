0efcf126068e1c41d12b894dc706caf76d78349f
Andrew <chenpoanandrew@gmail.com>
2015-07-30 15:05:10 +0800

    抽取appbar data flow

--- devServer/routes/index.js
+++ devServer/routes/index.js
@@ -3,31 +3,37 @@ var router = express.Router();
 var fs = require('fs');
 
 let productData = require('../data/product.root.js');
+let appbarData = require('../data/appbar.root.js');
 
 //建置static html page
 router.post('/', function(req, res, next) {
 
-	var path = 'public/' + req.body.name + '.html';
+    var path = 'public/' + req.body.name + '.html';
 
-	fs.unlink(path, function(){
+    fs.unlink(path, function() {
 
-		setTimeout(function(){
-			fs.writeFile( path, req.body.data, function (err) {
-				if (err) throw err;
-				console.log('Saved: ' + req.body.name + '.html' );
-			});
-		}, 1000);
+        setTimeout(function() {
+            fs.writeFile(path, req.body.data, function(err) {
+                if (err) throw err;
+                console.log('Saved: ' + req.body.name + '.html');
+            });
+        }, 1000);
 
-	});
+    });
 
-	res.json({ ok : 1});
+    res.json({
+        ok: 1
+    });
 });
 
 //讀取所有一般頁面
 router.get('/', function(req, res, next) {
 
-	if(req.query.name)
-		res.render(req.query.name, { data: productData['product_light_1'] } );
+    if (req.query.name)
+        res.render(req.query.name, {
+            data: productData['product_light_1'],
+            appbar: appbarData,
+        });
 });
 
 module.exports = router;
