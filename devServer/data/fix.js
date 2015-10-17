2fc943620a501a7b4af253b709a511a2111e985d
Andrew <chenpoanandrew@gmail.com>
2015-10-17 16:43:45 +0800

    update fix page

--- devServer/data/fix.js
+++ devServer/data/fix.js
@@ -1,15 +1,14 @@
 
 
 module.exports = [
-  { title: '裝修工程', describe: '', img: '' },
-  { title: '局部裝修', describe: '', img: '' },
-  { title: '防水抓漏', describe: '', img: '' },
-  { title: '水電工程', describe: '', img: '' },
-  { title: '地板工程', describe: '', img: '' },
-  { title: '木作工程', describe: '', img: '' },
-  { title: '門窗工程', describe: '', img: '' },
-  { title: '油漆工程', describe: '', img: '' },
-  { title: '照明工程', describe: '', img: '' },
-  { title: '空調工程', describe: '', img: '' },
-  { title: '尼作工程', describe: '', img: '' },
+  { title: '局部裝修', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/part.png' },
+  { title: '防水抓漏', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/water.png' },
+  { title: '水電工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/waterPower.png' },
+  { title: '地板工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/floor.png' },
+  { title: '木作工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/wood.png' },
+  { title: '門窗工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/window.png' },
+  { title: '油漆工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/color.png' },
+  { title: '照明工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/light.png' },
+  { title: '空調工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/air.png' },
+  { title: '泥作工程', describe: '工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..工程簡述..', img: 'asset/img/fix/dirt.png' },
 ];
