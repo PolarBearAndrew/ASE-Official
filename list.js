8fd6f8704c42558faf3ce29bf60c7468e9239877
PolarBearAndrew <PolarBearAndrew@users.noreply.github.com>
2015-10-31 17:13:44 +0800

    移除不必要的檔案

--- list.js
+++ list.js
@@ -2,15 +2,12 @@
 //在這個檔案中撰寫頁面名稱
 
 var list = [
-  // { page: 'index', data: '' },
   { page: 'ESCO', data: '' },
-  { page: 'Fix', data: '' },
   { page: 'home', data: '' },
   { page: 'LED', data: '' },
   { page: 'Money', data: '' },
   { page: 'PV', data: '' },
   { page: 'PVset', data: '' },
-  { page: 'Smart', data: '' },
   { page: 'Smart-home', data: '' },
   { page: 'Smart-energy', data: '' },
   { page: 'Smart-enterprise', data: '' },
